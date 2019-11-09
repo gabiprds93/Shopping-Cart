0885cf1924a7541ddf1217dd603666559c2a06bd
Gabriela Paredes Paredes <gabiprds.93@gmail.com>
2019-11-09 05:09:07 -0500

    styles of the submit button component

--- src/Components/SubmitButton.js
+++ src/Components/SubmitButton.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: #F7F7F7;
+  border: 1px solid #DDDDDD;
+  box-sizing: border-box;
+  border-radius: 4px;
+  font-weight: 600;
+  font-size: 16px;
+  height: 48px;
+  line-height: 24px;
+  color: #C1C1C1;
+`;
 
 const SubmitButton = () => {
-    return (
-        <button type='button'>COMPLETE ORDER</button>
-    )
+  return (
+    <Button type='button'>COMPLETE ORDER</Button>
+  )
 }
 
 export default SubmitButton
