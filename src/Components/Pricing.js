98851e0772ea85ce18251fe662ade391434c9a88
Gabriela Paredes Paredes <gabiprds.93@gmail.com>
2019-11-08 20:48:28 -0500

    base structure of the pricing component

---
+++ src/Components/Pricing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Pricing = () => {
+  return (
+      <div>
+        <div>
+          <span>Products</span>
+          <span>$0.00</span>
+        </div>
+        <div>
+          <span>Shipping cost</span>
+          <span>$0.00</span>
+        </div>
+        <div>
+          <span>Taxes</span>
+          <span>$0.00</span>
+        </div>
+        <div>
+          <span>Total</span>
+          <span>$0.00</span>
+        </div>
+      </div>
+  )
+}
+
+export default Pricing
