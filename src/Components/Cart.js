e1173a52fce927b765cea9770f29ba86ad690af3
Gabriela Paredes Paredes <gabiprds.93@gmail.com>
2019-11-09 04:40:56 -0500

    styles of the cart component

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import imgCart from '../Assets/img/img-cart.png'
 
+const Container = styled.div`
+  align-items: center;
+  border-radius: 4px;
+  display: flex;
+  flex-direction: column;
+  font-family: Open Sans;
+  justify-content: center;
+  padding: 0 60px;
+  text-align: center;
+`;
+
+const H2 = styled.h2`
+  line-height: 32px;
+  margin: 17px 0 8px;
+`;
+
+const Text = styled.span`
+  font-size: 16px;
+  line-height: 24px;
+`;
+
 const Cart = () => {
   return (
-      <div>
-        <img alt='cart' src={imgCart} />
-        <h2>Your cart is empty</h2>
-        <p>Seems like you haven’t chosen what to buy...</p>
-      </div>
+    <Container>
+      <img alt='cart' src={imgCart} />
+      <H2>Your cart is empty</H2>
+      <Text>Seems like you haven’t chosen what to buy...</Text>
+    </Container>
   )
 }
 
