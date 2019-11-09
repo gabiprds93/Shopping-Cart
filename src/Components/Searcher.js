0df4568a4cb3a18bbc2669b6f4a19df955c5469c
Gabriela Paredes Paredes <gabiprds.93@gmail.com>
2019-11-09 04:07:17 -0500

    styles of the searcher component

--- src/Components/Searcher.js
+++ src/Components/Searcher.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const InputContainer = styled.div`
+  & input {
+    border: 1px solid #DDDDDD;
+    box-sizing: border-box;
+    border-radius: 4px;
+    font-family: Open Sans;
+    font-size: 16px;
+    height: 48px;
+    line-height: 24px;
+    padding: 12px 16px;
+  }
+`;
 
 const Searcher = () => {
-    return (
-        <input type='text' />
-    )
+  return (
+    <InputContainer>
+      <input type='text' placeholder='Search Products'/>
+    </InputContainer>
+  )
 }
 
 export default Searcher
