162d39a5d16708c6b5c93b5fd5c312806aac90e3
Gabriela Paredes Paredes <gabiprds.93@gmail.com>
2019-11-08 20:30:40 -0500

    base structure of the delivery date component

---
+++ src/Components/DeliveryDate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import imgCar from '../Assets/img/img-car.png'
+
+const DeliveryDate = () => {
+  return (
+      <div>
+        <img alt='car' src={imgCar} />
+        <span>Buy now and get it by 05/24/19</span>
+      </div>
+  )
+}
+
+export default DeliveryDate
