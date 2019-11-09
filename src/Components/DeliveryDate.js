caaac2b51059c816f3686731951dd080f6af7acd
Gabriela Paredes Paredes <gabiprds.93@gmail.com>
2019-11-09 04:55:31 -0500

    styles of the delivery date component

--- src/Components/DeliveryDate.js
+++ src/Components/DeliveryDate.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import imgCar from '../Assets/img/img-car.png'
 
+const Container = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+`;
+
+const Text = styled.span`
+  font-size: 16px;
+  line-height: 24px;
+  padding: 0 0 0 12px;
+`;
+
 const DeliveryDate = () => {
   return (
-      <div>
-        <img alt='car' src={imgCar} />
-        <span>Buy now and get it by 05/24/19</span>
-      </div>
+    <Container>
+      <img alt='car' src={imgCar} />
+      <Text>Buy now and get it by 05/24/19</Text>
+    </Container>
   )
 }
 
