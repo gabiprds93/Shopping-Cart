202b761d5433d9f2d71f420008913d32385dd762
Gabriela Paredes Paredes <gabiprds.93@gmail.com>
2019-11-09 11:56:50 -0500

    styles from the thanks page

--- src/Pages/ThankYouPage.js
+++ src/Pages/ThankYouPage.js
@@ -1,19 +1,46 @@
 import React from 'react';
 import { Link } from "@reach/router"
+import styled from 'styled-components';
 
 import imgSuccess from '../Assets/img/img-success.png';
 
+const Container = styled.div`
+  align-items: center; 
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: center;
+`;
+
+const H1 = styled.h1`
+  font-size: 28px;
+  line-height: 42px;
+  margin: 0 0 8px;
+`;
+
+const Text = styled.span`
+  font-size: 16px;
+  line-height: 24px;
+  margin: ${({ className }) => (className === 'link' ? '8px 0 48px' : '8px 0')};
+  & > a {
+    color: #0500FF;
+    text-decoration: none;
+  }
+`;
+
 const ThankYouPage = () => {
-    return (
-        <div>
-          <h1>Thank you</h1>
-          <p>Your order P0001 has been registered</p>
-          <Link to='/'>Continue shopping</Link>
-          <div>
-            <img alt='success' src={imgSuccess} />
-          </div>
-        </div>
-    )
+  return (
+    <Container>
+      <H1>Thank you</H1>
+      <Text>Your order <b>P0001</b> has been registered</Text>
+      <Text className='link'>
+        <Link to='/'>Continue shopping</Link>
+      </Text>
+      <div>
+        <img alt='success' src={imgSuccess} />
+      </div>
+    </Container>
+  )
 }
 
 export default ThankYouPage
