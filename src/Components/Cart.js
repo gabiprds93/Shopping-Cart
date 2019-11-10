46e03c1f295a33ab37ebd7449b305f3b82ba7a03
Gabriela Paredes Paredes <gabiprds.93@gmail.com>
2019-11-10 14:42:05 -0500

    filter data

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -4,6 +4,7 @@ import { gql } from 'apollo-boost';
 import { Query } from 'react-apollo';
 
 import imgCart from '../Assets/img/img-cart.png'
+import Product from './Product'
 
 const Container = styled.div`
   align-items: center;
@@ -26,13 +27,15 @@ const Text = styled.span`
   line-height: 24px;
 `;
 
-const ProductsQuery = () => {
+const ProductsQuery = ({value}) => {
   return (
     <Query query={gql`
       {
         products {
           id
+          image
           name
+          price
         }
       }
     `}
@@ -41,9 +44,18 @@ const ProductsQuery = () => {
         if(loading) return <p>Loading...</p>
         if(error) return <p>Error</p>
 
-        return data.products.map(product => {
-          return <p key={product.id}>{product.name}</p>
+        let filterProducts = data.products.filter(product => {
+          return product.name.toLowerCase().includes(value.toLowerCase()) 
+        }).map(product => {
+          return <Product key={product.id} values={product} />
         })
+        console.log(filterProducts)
+        if(filterProducts.length){
+          return filterProducts
+        }
+        else{
+          return <p>No results</p>
+        }
       }}
     </Query>
   );
@@ -53,10 +65,16 @@ const Cart = ({ value }) => {
   console.log(value)
   return (
     <Container>
-      <img alt='cart' src={imgCart} />
-      <H2>Your cart is empty</H2>
-      <Text>Seems like you haven’t chosen what to buy...</Text>
-      <ProductsQuery/>
+      {value !== '' ? 
+        <ProductsQuery value={value}/>
+      :
+        <React.Fragment>
+          <img alt='cart' src={imgCart} />
+          <H2>Your cart is empty</H2>
+          <Text>Seems like you haven’t chosen what to buy...</Text>
+        </React.Fragment>
+      }
+      
     </Container>
   )
 }
