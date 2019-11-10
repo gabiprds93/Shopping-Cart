697c5f90b0c4d97765c782bbf8aaf2e3c4f1a099
Gabriela Paredes Paredes <gabiprds.93@gmail.com>
2019-11-09 21:06:23 -0500

    styles of the order page component

--- src/Pages/OrderPage.js
+++ src/Pages/OrderPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Searcher from '../Components/Searcher';
 import Cart from '../Components/Cart';
@@ -6,19 +7,52 @@ import DeliveryDate from '../Components/DeliveryDate';
 import Pricing from '../Components/Pricing';
 import SubmitButton from '../Components/SubmitButton';
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+const InputContainer = styled.div`
+  margin: 0 0 16px;
+  width: 45%;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: space-between;
+
+  & > div {
+    width: 45%;
+  }
+
+  & > div:first-child {
+    margin: 0 5% 0 0;
+  }
+
+  & > div:last-of-type {
+    & > div {
+      margin: 0 0 16px;
+    }
+  }
+`;
+
 const OrderPage = () => {
   return (
-    <div>
-      <Searcher />
-      <div>
+    <Container>
+      <InputContainer>
+        <Searcher />
+      </InputContainer>
+      <Wrapper>
         <Cart />
         <div>
           <DeliveryDate />
           <Pricing />
           <SubmitButton />
         </div>
-      </div>
-    </div>
+      </Wrapper>
+    </Container>
   )
 }
 
