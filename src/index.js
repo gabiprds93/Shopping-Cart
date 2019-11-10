390dd09e5a6e8adc80b9170af35b2e8999b219f9
Gabriela Paredes Paredes <gabiprds.93@gmail.com>
2019-11-09 20:26:37 -0500

    page container styles

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from "@reach/router";
+import styled from 'styled-components';
 
+import './Assets/css/styles.css'
 import OrderPage from './Pages/OrderPage';
 import ThankYouPage from './Pages/ThankYouPage';
 
+const Container = styled.div`
+  background: #E5E5E5;
+  color: #333333;
+  height: 100vh;
+  & > div {
+    box-sizing: border-box;
+    height: 100%;
+    padding: 68px 120px 48px;
+  }
+`
 const App = (
-  <Router>
-    <OrderPage path='/' />
-    <ThankYouPage path='thanks' />
-  </Router>
+  <Container>
+    <Router>
+      <OrderPage path='/' />
+      <ThankYouPage path='thanks' />
+    </Router>
+  </Container>
 )
 
 ReactDOM.render(App, document.getElementById('root'));
